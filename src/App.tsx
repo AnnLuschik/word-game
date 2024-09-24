b84b8308575e98e71a1edb0686978d59158d4888
Hanna Lushchyk <iniusha@gmail.com>
2024-09-24 11:25:58 +0400

    feat: detect and synchronize the most up-to-date tab, show a dialog for outdated tabs with a reload option

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import useLockBodyScroll from 'react-use/lib/useLockBodyScroll';
 import { LevelScreen } from '@/components/screens/LevelScreen';
 import { SuccessScreen } from './components/screens/SuccessScreen';
+import { Button } from './components/elements/Button';
 import { useLevelData } from '@/hooks/useLevelData';
 import './App.css';
 
@@ -12,6 +14,43 @@ function App() {
     return savedLevel ? parseInt(savedLevel, 10) : 1;
   });
   const [success, setSuccess] = useState<boolean>(false);
+  const [isActiveTab, setIsActiveTab] = useState<boolean>(true);
+  const [isDialogVisible, setIsDialogVisible] = useState<boolean>(false);
+
+  useLockBodyScroll(isDialogVisible && !isActiveTab);
+
+  const setTabActive = useCallback(() => {
+    const now = Date.now();
+    localStorage.setItem('lastActiveTab', String(now));
+  }, []);
+
+  const reloadPage = () => window.location.reload();
+
+  useEffect(() => {
+    const lastActiveTab = localStorage.getItem('lastActiveTab');
+    const now = Date.now();
+
+    if (lastActiveTab && parseInt(lastActiveTab, 10) > now) {
+      setIsActiveTab(false);
+      setIsDialogVisible(true);
+    } else {
+      setTabActive();
+    }
+
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === 'lastActiveTab') {
+        const timestamp = parseInt(event.newValue || '', 10);
+        if (timestamp > now) {
+          setIsActiveTab(false);
+          setIsDialogVisible(true);
+        }
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, [setTabActive]);
 
   const { data, error, loading } = useLevelData(getLevelForFetch(currentLevel));
 
@@ -37,6 +76,18 @@ function App() {
 
   return (
     <div className='App'>
+      {isDialogVisible && !isActiveTab && (
+        <div className='overlay'>
+          <div className='dialog'>
+            <p>
+              Похоже, игра открыта в нескольких вкладках браузера. Чтобы
+              продолжить играть в этой вкладке, обновите страницу.
+            </p>
+            <Button text='Обновить' onClick={reloadPage} />
+          </div>
+        </div>
+      )}
+
       {success ? (
         <SuccessScreen level={currentLevel} onClick={openNewLevel} />
       ) : (
